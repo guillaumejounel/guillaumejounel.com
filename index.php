9669a0eabf503ed89576461b2eadee2ec5f091b4
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2017-03-15 11:05:44 +0100

    Added fontawesome

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <html>
 <head>
-  <meta charset="UTF-8">
-  <title>Guillaume Jounel - Who am I</title>
-  <script language="javascript" type="text/javascript" src="js/p5.js"></script>
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <script language="javascript" type="text/javascript" src="js/particle_system.js"></script>
-  <style>  </style>
+    <meta charset="UTF-8">
+    <title>Guillaume Jounel - Who am I</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script language="javascript" type="text/javascript" src="js/p5.js"></script>
+    <script language="javascript" type="text/javascript" src="js/particle_system.js"></script>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
     <div id="bg"></div>
-    <p style="font-family:'Myriad Set Pro'; margin:30px 0px 30px 0px;font-size:250%; position:relative; text-align:center; width:100%">Guillaume <span style="font-weight:100">Jounel</span></p>
+    <p id="name">Guillaume <span>Jounel</span></p>
     <div class="blurred">
-        <p style="font-family:'Myriad Set Pro';">Hi! I'm Guillaume. <?php echo $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?> :)<br/>
-        I'm currently looking for an internship, please feel free to contact me.<br/>
-        See you soon! :)</p>
-        <div class="blur"></div>
-    </div>
-    <div class="content">
-        <div class="column">
-            <div class="article"></div>
-            <div class="article"></div>
+        <p>Hi! I'm Guillaume. <?php echo $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?> :)<br/>
+            I'm currently looking for an internship, please feel free to contact me.<br/>
+            See you soon! :)</p>
+            <div class="blur"></div>
         </div>
-        <div class="column">
-            <div class="article"></div>
-            <div class="article"></div>
+        <div class="content">
+            <div class="column">
+                <div class="article"></div>
+                <div class="article"></div>
+            </div>
+            <div class="column">
+                <div class="article"></div>
+                <div class="article"></div>
+            </div>
+            <div class="column">
+                <div class="article"></div>
+                <div class="article"></div>
+            </div>
         </div>
-        <div class="column">
-            <div class="article"></div>
-            <div class="article"></div>
-        </div>
-    </div>
-</body>
-</html>
+    </body>
+    </html>
