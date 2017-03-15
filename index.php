d519c4d5156ec1d2ec285ecd91758c59fd2b4525
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2017-03-15 11:30:31 +0100

    Fixed a incompatibility of backdrop filter

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script language="javascript" type="text/javascript" src="js/p5.js"></script>
     <script language="javascript" type="text/javascript" src="js/particle_system.js"></script>
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
 </head>
 <body>
     <div id="bg"></div>
     <p id="name">Guillaume <span>Jounel</span></p>
     <div class="blurred">
-        <p>Hi! I'm Guillaume. <?php echo $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?> :)<br/>
+        <p><span id="status"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Looking For a Six-Month Software Engineer Internship (Sept 2017 - Febr 2018)</span>
+            Hi! I'm Guillaume. <?php echo $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?> :)<br/>
             I'm currently looking for an internship, please feel free to contact me.<br/>
             See you soon! :)</p>
             <div class="blur"></div>
