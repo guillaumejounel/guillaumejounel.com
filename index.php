f3b0a01c26b27ae9c0c6615f287e1049fabaab82
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2017-03-16 01:16:26 +0100

    User can now switch from one language to another

--- index.php
+++ index.php
@@ -9,38 +9,11 @@
     <link rel="stylesheet" href="css/font-awesome.min.css">
 </head>
 <body>
-    <script>
-    var duration = 0;
-    $("#language").css("font-weight", "bold")
-    var language = "<?php echo $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?>"
-    function loadContent() {
-        if (language == "fr") {
-            $("#fr").css("font-weight", "bold")
-            $("#en").css("font-weight", "normal")
-        } else {
-            $("#fr").css("font-weight", "normal")
-            $("#en").css("font-weight", "bold")
-        }
-        $.getJSON("content/general.json", function(json) {
-            $.each(json, function(i, value) {
-                if (language == "fr")
-                    $(value.id).fadeOut(duration, function() { $(this).html(value.fr) }).fadeIn()
-                else
-                    $(value.id).fadeOut(duration, function() { $(this).html(value.en) }).fadeIn()
-            });
-        });
-    }
-    function update(lan) {
-        duration = 300;
-        language = lan;
-        loadContent()
-    }
-    loadContent()
-    </script>
+
     <div id="bg"></div>
     <p id="name">Guillaume <span>Jounel</span></p>
     <div class="blurred">
-        <p><span id="language"><span id="en">en</span> &bull; <span id="fr">fr</span></span>
+        <p><span id="language"><span id="en">en</span> ~ <span id="fr">fr</span></span>
             <span id="status"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Looking for a Six-Month Software Engineer Internship (Sept 2017 - Feb 2018)</span>
             <span id="introduction">Hi! I'm Guillaume.<br/>
             I'm currently looking for an internship, please feel free to contact me. Fast learner, creative & self-challenging!<br/>
@@ -61,5 +34,40 @@
                 <div class="article"></div>
             </div>
         </div>
+
+        <script>
+        var duration = 0;
+
+        var language = "<?php echo $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?>"
+        function loadContent() {
+            if (language == "fr") {
+                $("#fr").css("font-weight", "bold")
+                $("#en").css("font-weight", "normal")
+            } else {
+                $("#fr").css("font-weight", "normal")
+                $("#en").css("font-weight", "bold")
+            }
+            $.getJSON("content/general.json", function(json) {
+                $.each(json, function(i, value) {
+                    if (language == "fr")
+                        $(value.id).fadeOut(duration, function() { $(this).html(value.fr) }).fadeIn()
+                    else
+                        $(value.id).fadeOut(duration, function() { $(this).html(value.en) }).fadeIn()
+                });
+            });
+        }
+        function update(lan) {
+            duration = 300;
+            language = lan;
+            loadContent()
+        }
+        loadContent()
+        $("#fr").click(function() {
+            update("fr")
+        });
+        $("#en").click(function() {
+            update("en")
+        });
+        </script>
     </body>
     </html>
