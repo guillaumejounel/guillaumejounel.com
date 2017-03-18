93bc36a4e95f616a68fc74b15b7b4eb44f46b63f
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2017-03-18 18:34:24 +0100

    Added an image background if no JS support

--- js/content.js
+++ js/content.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-    $('#fixed').css('position', 'fixed');
-    $('#content').css('top', $('#fixed').outerHeight());
+    $('#fixed').css('position', 'fixed')
+    $('body').css('background', 'white')
+    $('#content').css('top', $('#fixed').outerHeight())
     var duration = 0;
     var url = window.location.toString().split("/");
     var language = (url[url.indexOf("guillaumejounel.com")+1] || navigator.language || navigator.userLanguage).slice(0,2) == "fr" ? "fr" : "en"
