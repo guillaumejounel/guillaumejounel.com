0cd75f9f32d5b8a371c4f5421fb4051246e16117
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2017-03-19 16:52:55 +0100

    Optimized img & js files

--- js/content.js
+++ js/content.js
@@ -1,53 +1,18 @@
-$(document).ready(function() {
-    $('body').css('background', 'white')
-    var duration = 0;
-    var url = window.location.toString().split("/");
-    var language = (url[url.indexOf("guillaumejounel.com")+1] || navigator.language || navigator.userLanguage).slice(0,2) == "fr" ? "fr" : "en"
-
-    function loadContent() {
-        if (language == "fr") {
-            $("#fr").css("font-weight", "bold")
-            $("#en").css("font-weight", "normal")
-        } else {
-            $("#fr").css("font-weight", "normal")
-            $("#en").css("font-weight", "bold")
-        }
-        $.getJSON("content/general.json", function(json) {
-            $.each(json, function(id, value) {
-                if (language == "fr")
-                    $(id).fadeOut(duration, function() { $(this).html(value.fr) }).fadeIn()
-                else
-                    $(id).fadeOut(duration, function() { $(this).html(value.en) }).fadeIn()
-            });
-        });
-
-    }
-
-    $.getJSON("content/articles.json", function(json) {
-        var i = 0;
-        $.each(json, function(id, value) {
-            if (i<3) {
-                if (language == "fr")
-                    $("#content").append("<article>"+value.fr.title+"</article>")
-                else
-                    $("#content").append("<article>"+value.en.title+"</article>")
-                i+=1
-            }
-        });
-    })
-    function update(lan) {
-        duration = 300;
-        language = lan;
-        loadContent()
-    }
-
-    $("#language span").click(function() {
-        id = $(this).attr('id')
-        update(id)
-        window.history.pushState(document.title,document.title,id);
-        return false
-    });
-
-    loadContent()
-
-});
+$(document).ready(function(){$('body').css('background','white')
+var duration=0;var url=window.location.toString().split("/");var language=(url[url.indexOf("guillaumejounel.com")+1]|| navigator.language||navigator.userLanguage).slice(0,2)=="fr"?"fr":"en"
+function loadContent(){if(language=="fr"){$("#fr").css("font-weight","bold")
+$("#en").css("font-weight","normal")}else{$("#fr").css("font-weight","normal")
+$("#en").css("font-weight","bold")}
+$.getJSON("content/general.json",function(json){$.each(json,function(id,value){if(language=="fr")
+$(id).fadeOut(duration,function(){$(this).html(value.fr)}).fadeIn()
+else
+$(id).fadeOut(duration,function(){$(this).html(value.en)}).fadeIn()});});}
+$.getJSON("content/articles.json",function(json){var i=0;$.each(json,function(id,value){if(i<3){if(language=="fr")
+$("#content").append("<article>"+value.fr.title+"</article>")
+else
+$("#content").append("<article>"+value.en.title+"</article>")
+i+=1}});})
+function update(lan){duration=300;language=lan;loadContent()}
+$("#language span").click(function(){id=$(this).attr('id')
+update(id)
+window.history.pushState(document.title,document.title,id);return false});loadContent()});
---
+++ js/particle_system_plain.js
@@ -0,0 +1,69 @@
+var particles = []
+
+function Particle(location) {
+    this.location = location.copy()
+    this.velocity = createVector(random(-0.5,0.5), random(-0.5,0.5))
+
+    this.run = function() {
+        this.update()
+        this.connect()
+    }
+
+    this.update = function() {
+        this.location.add(this.velocity)
+        if(this.location.x < 0) {
+            this.velocity.x *= -1
+        }
+        if(this.location.x > width) {
+            this.velocity.x *= -1
+        }
+        if(this.location.y < 0) {
+            this.velocity.y *= -1
+        }
+        if(this.location.y > height) {
+            this.velocity.y *= -1
+        }
+    }
+
+    this.distance = function (particle) {
+        distance = p5.Vector.sub(this.location, particle.location)
+        return distance.mag()
+    }
+
+    this.connect = function() {
+
+        //fill(100,0,0)
+        beginShape()
+        for (j=0; j<particles.length-1; j++) {
+            temp = this.distance(particles[j])
+            if (temp < minimum && temp > 10) {
+                vertex(this.location.x, this.location.y, 0)
+                vertex(particles[j].location.x, particles[j].location.y, 0)
+            }
+        }
+        endShape()
+    }
+}
+
+var minimum = 65
+function setup() {
+    var canvas = createCanvas(windowWidth,windowHeight, WEBGL)
+    canvas.parent('bg');
+    for (i=1; i<height*width/2000; i++) {
+        particles.push(new Particle(createVector(random(width), random(height))))
+    }
+}
+
+function draw() {
+    particles[0].location.x = mouseX;
+    particles[0].location.y = mouseY;
+    translate(-width/2,-height/2,0);
+    background(255, 255, 255, 0)
+    for (i=particles.length-1; i>-1; i--) {
+        particles[i].run()
+    }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
