f98e0013a5b162f3903e8dc11fea7ea0e0799306
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2017-03-21 16:31:07 +0100

    Added links to social profiles & GitHub repo

--- index.php
+++ index.php
@@ -14,7 +14,10 @@ $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) == "fr" ? "fr" : "en"; ?>
 </head>
 <body>
     <div id="bg"></div>
-    <header><p>Guillaume <span>Jounel</span></p></header>
+    <header>
+        <p>Guillaume <span>Jounel</span></p>
+        <div id="social"><a href="mailto:gjounel@gmail.com"><i class='fa fa-envelope-o' aria-hidden='true'></i></a><a href="https://github.com/guillaumejounel" target="_blank"><i class='fa fa-github' aria-hidden='true'></i></a><a href="https://linkedin.com/in/guillaumejounel" target="_blank"><i class='fa fa-linkedin' aria-hidden='true'></i></a><a href="https://facebook.com/guillaumejounel" target="_blank"><i class='fa fa-facebook' aria-hidden='true'></i></a><a href="https://twitter.com/guillaumejounel" target="_blank"><i class='fa fa-twitter' aria-hidden='true'></i></a></div>
+    </header>
     <div id="blurred">
         <section>
             <p id="title"><span id="language"><a href="en"><span id="en" <?php if($lang=="en") echo "style='font-weight:bold'"?>>en</span></a> ~ <a href="fr"><span id="fr" <?php if($lang=="fr") echo "style='font-weight:bold'"?>>fr</span></a></span><span id="status"><?php echo $json["#status"][$lang]; ?></span></p>
