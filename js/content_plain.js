1c3b235fbba89316048c15fc10e9b01c2343abb6
Guillaume Jounel <guillaume.jounel@etu.utc.fr>
2017-03-22 19:06:07 +0100

    Minor fix

--- js/content_plain.js
+++ js/content_plain.js
@@ -30,14 +30,14 @@ $(document).ready(function() {
                         $("article#"+id+" p").html(value.fr.resume)
                         $("article#"+id+" div.keywords").empty()
                         for (var i = 0; i < value.fr.keywords.length; i++) {$("article#"+id+" div.keywords").append("<span>"+value.fr.keywords[i]+"</span>")}
-                    }).animate({opacity:1}, 100)
+                    }).animate({opacity:0.8}, 100)
                 } else {
                     $("article#"+id).animate({opacity:0},100, function() {
                         $("article#"+id+" h3").html(value.en.title+ "<span class='delete'>X</span>");
                         $("article#"+id+" p").html(value.en.resume)
                         $("article#"+id+" div.keywords").empty()
                         for (var i = 0; i < value.en.keywords.length; i++) {$("article#"+id+" div.keywords").append("<span>"+value.en.keywords[i]+"</span>")}
-                    }).animate({opacity:1}, 100)
+                    }).animate({opacity:0.8}, 100)
 
                 }
             });
